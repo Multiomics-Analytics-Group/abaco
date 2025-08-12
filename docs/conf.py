05ac7164c4032e8100ace47945042747a1589214
Angel L. P <59593766+angelphanth@users.noreply.github.com>
2025-08-12 20:34:51 +0200

    temp imgs to be replaced with abaco logo, favicon

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 from importlib import metadata
 
@@ -25,9 +15,6 @@ release = PACKAGE_VERSION
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",  # Core extension for generating documentation from docstrings
     "sphinx.ext.autodoc.typehints",  # Automatically document type hints in function signatures
@@ -56,18 +43,10 @@ nb_execution_raise_on_error = True
 # Rendering
 nb_merge_streams = True
 
-# https://myst-nb.readthedocs.io/en/latest/authoring/custom-formats.html#write-custom-formats
-# ! if you use it, then you cannot directly execute the notebook in the browser in colab
-# (the file needs to be fetched from the repository)
-# just keep both syncing it using papermill
-# nb_custom_formats = {".py": ["jupytext.reads", {"fmt": "py:percent"}]}
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+# Ignore
 exclude_patterns = [
     "_build",
     "Thumbs.db",
@@ -75,7 +54,6 @@ exclude_patterns = [
     "jupyter_execute",
     "conf.py",
 ]
-
 
 # Intersphinx options
 intersphinx_mapping = {
@@ -88,14 +66,9 @@ intersphinx_mapping = {
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-# See:
-# https://github.com/executablebooks/MyST-NB/blob/master/docs/conf.py
-# html_title = ""
 html_theme = "sphinx_book_theme"
-html_logo = "assets/abaco.svg"
-html_favicon = "assets/abaco.svg"
+html_logo = "https://raw.githubusercontent.com/Multiomics-Analytics-Group/abaco/refs/heads/main/docs/figures/overview.png"
+html_favicon = "https://raw.githubusercontent.com/Multiomics-Analytics-Group/abaco/refs/heads/main/docs/figures/overview.png"
 html_theme_options = {
     "github_url": "https://github.com/Multiomics-Analytics-Group/abaco",
     "repository_url": "https://github.com/Multiomics-Analytics-Group/abaco",
@@ -108,27 +81,11 @@ html_theme_options = {
     "use_download_button": True,
     "launch_buttons": {
         "colab_url": "https://colab.research.google.com"
-        #     "binderhub_url": "https://mybinder.org",
-        #     "notebook_interface": "jupyterlab",
     },
     "navigation_with_keys": False,
 }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
-
-# html_js_files = [
-#     "require.min.js",
-#     "custom.js",
-# ]
-
 # -- Setup for sphinx-apidoc -------------------------------------------------
-
-# Read the Docs doesn't support running arbitrary commands like tox.
-# sphinx-apidoc needs to be called manually if Sphinx is running there.
-# https://github.com/readthedocs/readthedocs.org/issues/1139
 
 if os.environ.get("READTHEDOCS") == "True":
     from pathlib import Path
